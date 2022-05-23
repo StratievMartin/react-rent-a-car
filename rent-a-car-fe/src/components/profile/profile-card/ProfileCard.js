a0e2c8ee38295a88aff008158ddc1edf880dd209
Martin Stratiev <martinstratiev1@gmail.com>
2022-05-23 12:33:02 +0300

    minor design changes in rentCard

--- rent-a-car-fe/src/components/profile/profile-card/ProfileCard.js
+++ rent-a-car-fe/src/components/profile/profile-card/ProfileCard.js
@@ -10,7 +10,6 @@ export const ProfileCard = ({ user }) => {
             getUserRents(user._id)
                 .then((res) => {
                     setUserRents(res.data);
-                    console.log(res.data);
                 })
                 .catch((err) => {
                     console.log(err);
@@ -19,7 +18,7 @@ export const ProfileCard = ({ user }) => {
     }, []);
     return (
         <div class="flex justify-center m-5">
-            <div class="bg-blue-200 rounded-xl ">
+            <div class="bg-blue-100 rounded-xl ">
                 <div class="p-5 text-left text-lg">
                     <h2>Full Name: {user.fullName}</h2>
                     <h2>Email: {user.email}</h2>
@@ -31,10 +30,12 @@ export const ProfileCard = ({ user }) => {
                         </button>
                     </Link>
                 </div>
-                <div class=" bg-blue-100 rounded-b-xl">
+                <div class=" bg-gray-500 rounded-b-xl">
                     <div>
                         {userRents.length > 0 ? (
-                            <RentCard userRents={userRents} />
+                            <>
+                                <RentCard userRents={userRents} />
+                            </>
                         ) : (
                             <div class="bg-red-200">No rents yet</div>
                         )}
--- rent-a-car-fe/src/components/rent/rent-card/RentCard.js
+++ rent-a-car-fe/src/components/rent/rent-card/RentCard.js
@@ -1,12 +1,17 @@
-import { CarCard } from "../../cars/car-card/CarCard";
+import { CarCard } from '../../cars/car-card/CarCard';
 export const RentCard = ({ userRents }) => {
-    console.log(userRents);
     return (
         <div>
             <h2 class="font-bold mb-2">Your rents:</h2>
             <div class="grid lg:grid-cols-3 md:grid-cols-2">
                 {userRents.map((rent) => (
-                    <CarCard key={rent._id} car={rent.car}/>
+                    <div class="relative">
+                        <CarCard class=" z-0" key={rent._id} car={rent.car} />
+                        <div class="bg-red-400 p-1 text-white absolute z-50 top-5 left-5 rounded-tl-xl rounded-br-xl">
+                            <h2>Start Date: {rent.startDate}</h2>
+                            <h2>End Date: {rent.endDate}</h2>
+                        </div>
+                    </div>
                 ))}
             </div>
         </div>
